71ef75dd5559e64d22461237850803904815b662
Vladislav Stukov <>
2024-11-03 20:59:13 +0100

    Last updated version

--- app/imagesets-list/new/capturing-imageset-images/index.tsx
+++ app/imagesets-list/new/capturing-imageset-images/index.tsx
@@ -3,76 +3,67 @@ import { getImageSetFolderPath, updateImageSet } from "@/utils/ImageSet.utils";
 import { CameraView } from "expo-camera";
 import * as FileSystem from "expo-file-system";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import React, { useCallback, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const CaptureImagesScreen = () => {
   const [imageCount, setImageCount] = useState(0);
-  const isCapturingRef = useRef<boolean>(false);
+  const cameraRef = useRef<CameraView>(null);
+  const isCapturingRef = useRef<Boolean>(false);
   const router = useRouter();
   const { imageSetJson } = useLocalSearchParams<{ imageSetJson: string }>();
   const imageSetRef = useRef<ImageSet>();
   const imageSetFolderRef = useRef<string | undefined>();
-  const photoLoopIntervalRef = useRef<NodeJS.Timeout>();
 
-  const cameraRef = useRef<CameraView>(null);
-
-  const takePicture = useCallback(async () => {
-    console.log("takePicture called");
+  const takePicturesLoop = async () => {
+    console.log("takePicturesLoop start");
     if (cameraRef.current && isCapturingRef.current) {
       try {
-        console.log("Attempting to capture photo...");
-        const photo = await cameraRef.current.takePictureAsync();
+        const photo = await cameraRef.current.takePictureAsync({
+          skipProcessing: true,
+        });
         if (photo) {
           const fileName = `${
             imageSetFolderRef.current
-          }/photo_${Date.now()}.jpg`;
-          console.log("Photo captured, saving to:", fileName);
+          }photo_${Date.now()}.jpg`;
           await FileSystem.moveAsync({
             from: photo.uri,
             to: fileName,
           });
           console.log("Saved photo to:", fileName);
-          setImageCount((prevCount) => prevCount + 1);
+          setImageCount((prevCount) => prevCount + 1); // Increment image count
+          setTimeout(takePicturesLoop, 1000);
         }
       } catch (error) {
         console.error("Error capturing photo:", error);
       }
-    } else {
-      console.log("Camera is not ready or capturing has stopped.");
     }
+  };
+
+  useEffect(() => {
+    const imageSet = ImageSet.fromJSON(imageSetJson);
+    console.log(imageSet);
+    imageSetRef.current = imageSet;
+    imageSetFolderRef.current = getImageSetFolderPath(imageSet);
+
+    return () => {
+      isCapturingRef.current = false;
+    };
   }, []);
 
   const onCameraReady = () => {
-    console.log("Camera is ready.");
-    const imageSet = ImageSet.fromJSON(imageSetJson);
-    console.log("ImageSet loaded from JSON:", imageSet);
-    imageSetRef.current = imageSet;
-    imageSetFolderRef.current = getImageSetFolderPath(imageSet);
-    console.log("ImageSet folder path set to:", imageSetFolderRef.current);
-
-    isCapturingRef.current = true;
-    console.log("Starting image capture loop...");
-    photoLoopIntervalRef.current = setInterval(takePicture, 1000);
-
-    return () => {
-      console.log("Cleaning up interval and stopping capture.");
-      isCapturingRef.current = false;
-      clearInterval(photoLoopIntervalRef.current);
-    };
+    setTimeout(() => {
+      isCapturingRef.current = true;
+      takePicturesLoop();
+    }, 5000);
   };
 
   const handleStopCapture = () => {
-    console.log("Stop capture button pressed.");
-    if (imageSetRef.current) {
-      imageSetRef.current.endTime = new Date();
-      console.log("Set ImageSet end time to:", imageSetRef.current.endTime);
-      updateImageSet(imageSetRef.current);
-      clearInterval(photoLoopIntervalRef.current);
-    }
+    console.log("Stop button pressed. Navigating to save screen.");
+    imageSetRef.current!.endTime = new Date();
+    updateImageSet(imageSetRef.current!);
 
     isCapturingRef.current = false;
-    console.log("Navigating back to image sets list and details.");
     router.dismissAll();
     router.push("/imagesets-list");
     router.push({ pathname: "/imageset-details", params: { imageSetJson } });
@@ -81,7 +72,7 @@ const CaptureImagesScreen = () => {
   return (
     <View style={styles.container}>
       <View style={styles.counterContainer}>
-        <Text style={styles.counterLabel}>Images Captured</Text>
+        <Text style={styles.counterLabel}>images captured</Text>
         <Text style={styles.counter}>{imageCount}</Text>
       </View>
 
@@ -89,9 +80,6 @@ const CaptureImagesScreen = () => {
         style={styles.camera}
         ref={cameraRef}
         onCameraReady={onCameraReady}
-        onMountError={(event) => {
-          console.error("Camera mount error:", event.message);
-        }}
       />
 
       <TouchableOpacity style={styles.stopButton} onPress={handleStopCapture}>
@@ -126,8 +114,8 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
   camera: {
-    width: 200,
-    height: 200,
+    width: 200, // Set the width of CameraView to make it smaller
+    height: 200, // Set the height of CameraView to make it smaller
     borderRadius: 8,
     overflow: "hidden",
     marginTop: 16,
