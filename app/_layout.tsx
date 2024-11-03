80314289ada982b8bb86805540ec16e09bc83aeb
Vladislav Stukov <>
2024-11-03 20:33:18 +0100

    Add logs. Add ErrorBoundary

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import ErrorBoundary from "@/components/ErrorBoundary";
 import { ImagesetsProvider } from "@/contexts/ImageSets.context";
 import { Stack } from "expo-router";
 import React from "react";
@@ -5,12 +6,14 @@ import { Provider as PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
-    <PaperProvider>
-      <ImagesetsProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
-      </ImagesetsProvider>
-    </PaperProvider>
+    <ErrorBoundary>
+      <PaperProvider>
+        <ImagesetsProvider>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+          </Stack>
+        </ImagesetsProvider>
+      </PaperProvider>
+    </ErrorBoundary>
   );
 }
---
+++ components/ErrorBoundary.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render shows the fallback UI.
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can log the error to an error reporting service here
+    console.error("ErrorBoundary caught an error", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <View style={styles.container}>
+          <Text style={styles.errorText}>Something went wrong.</Text>
+        </View>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+  errorText: {
+    fontSize: 18,
+    color: "red",
+  },
+});
+
+export default ErrorBoundary;
